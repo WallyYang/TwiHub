35b1bc012444639b445347134a0c76a514211364
hcz <h.cz@qq.com>
2018-10-27 18:45:50 -0400

    feat: implement mouse events

--- ts/popup.ts
+++ ts/popup.ts
@@ -1,3 +1,45 @@
 'use strict';
 
-// TODO: show a popup window w/ like button
+/// <reference path="./file_collection.ts"/>
+
+// TODO: move to config file?
+const url = 'http://laurence.tk:21027';
+
+const fileCollection = new FileCollection(<HTMLBodyElement> document.body);
+
+let initEvent = null;
+
+function mouseDown(event: MouseEvent) {
+    initEvent = event;
+}
+
+function mouseUp(event: MouseEvent) {
+    if (initEvent !== null) {
+        if (event.screenX === initEvent.screenX && event.screenY === initEvent.screenY) {
+            fileCollection.deselect();
+        } else {
+            fileCollection.select(
+                Math.min(event.screenY, initEvent.screenY),
+                Math.max(event.screenY, initEvent.screenY)
+            );
+
+            for (const file of fileCollection.files) {
+                let code = '';
+
+                for (const line of file.lines) {
+                    code += line.code + '\n';
+                }
+
+                const http = new XMLHttpRequest();
+
+                http.open('POST', url + '/like', true);
+                http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                http.setRequestHeader('X-LAURENCE', '6');
+                http.send('{"reop":"a","code":"b"}');
+            }
+        }
+    }
+}
+
+window.onmousedown = mouseDown;
+window.onmouseup = mouseUp;
