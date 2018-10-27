e6da63c5290434be430bf4c5bb6c8c0e0a2501e7
Wally Yang <wallyyang28@gmail.com>
2018-10-27 11:45:57 -0400

    chore: add gitignore

---
+++ js/code_structure.js
@@ -0,0 +1,15 @@
+'use strict';
+function getLine() {
+    // return:
+    // code, html node of the line
+}
+function getFile() {
+    // return:
+    // url
+    // parsed url (tree hash, blob, file name, ...),
+    // lines
+}
+function getAll() {
+    // return:
+    // all getFile() result in current webpage
+}
