2617f544c11349300a46cc7dc3aa76545d000f13
hcz <h.cz@qq.com>
2018-10-27 11:32:48 -0400

    add files

---
+++ ts/code_structure.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+function getLine() {
+    // return:
+    // code, html node of the line
+}
+
+function getFile() {
+    // return:
+    // url
+    // parsed url (tree hash, blob, file name, ...),
+    // lines
+}
+
+function getAll() {
+    // return:
+    // all getFile() result in current webpage
+}
---
+++ ts/like_popup.ts
@@ -0,0 +1,3 @@
+'use strict';
+
+// TODO: get selection, show a popup window w/ like button
