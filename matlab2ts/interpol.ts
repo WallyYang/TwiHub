3387e52a49368b2ad389b1ca3367888f7ca0581f
YuyiChang <changyuyi98@qq.com>
2018-10-27 16:24:56 -0400

    Wally(Feat): interpolation in typescript

---
+++ matlab2ts/interpol.ts
@@ -0,0 +1,42 @@
+//	qData = interpol(orgId, orgData, qId)
+//		orgId: column index
+//		orgData: number of likes
+//		qId: querry index (heatmap index)
+//		qData: heatmap height, normalize to 1
+function interpol(orgId: number[], orgData: number[], qId: number[]) {
+	let qData: number[] = [];	// declear output data qData
+
+	// Compute heatmap height at each point
+	for (let i in orgId) {
+		let qPt: number = qId[i];	// querry point
+
+		// Get left and right hand points
+		let lh_x: number = Math.floor(qPt);
+		let rh_x: number = Math.ceil(qPt);
+		let lh_y: number = orgData[lh_x];
+		let rh_y: number = orgData[rh_x];
+
+		let qDataPt = mSingleInterpl(lh_x, lh_y, rh_x, rh_y, qPt); // compute heatmap height at given point
+		qData[i] = qDataPt;	// assign to array
+	}
+
+	return qData; // return heatmap height list
+}
+
+// Calculate heatmap height at given point qPt using ratios
+function mSingleInterpl(x1: number, y1: number, x2: number, y2: number, qPt: number){
+	if (x1 == x2) {
+		return y1;
+	}
+	
+	let qDataPt: number = y1 + (qPt - x1) * (y2 - y1) / (x2 - x1);
+	
+	return qDataPt;
+}
+
+let colId: number[] = [1, 2, 3, 4, 5, 6];							// column index
+let numOfLikes: number[] = [0, 0, 10, 0, 2, 3];						// number of likes
+let hmId: number[] = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6];	// heatmap index
+
+
+document.body.innerHTML = JSON.stringify(interpol(colId, numOfLikes, hmId));
--- matlab_model/mInterpl.m
+++ matlab_model/mInterpl.m
@@ -3,14 +3,16 @@ function [qData] = mInterpl(orgId,orgData, qId)
 %   [qData] = mInterpl(orgId,orgData, qId)
     qData(1:length(qId)) = 0;
     
-for i = 2:length(qId)-1
+for i = 1:length(qId)
     % get querry pt (single)
     qPt = qId(i);
     % LH and RH pts
     lh_x = floor(qPt);  % x1
     rh_x = ceil(qPt);   % x2
-    lh_y = orgData(orgId==lh_x);    % y1
-    rh_y = orgData(orgId==rh_x);    % y2
+%     lh_y = orgData(orgId==lh_x);    % y1
+%     rh_y = orgData(orgId==rh_x);    % y2
+    lh_y = orgData(lh_x);    % y1
+    rh_y = orgData(rh_x);    % y2
     % Interpol to get data val at given pt
     qDataPt = mSingleInterpl(lh_x, lh_y, rh_x, rh_y, qPt);
     qData(i) = qDataPt;
