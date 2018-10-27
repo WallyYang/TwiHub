536e125ae99db3d9827bd3f6bf12d455a1e12b99
YuyiChang <changyuyi98@qq.com>
2018-10-27 14:03:38 -0400

    add interpolation calculation

--- matlab model/heatmap_manual.m
+++ matlab model/heatmap_manual.m
@@ -20,3 +20,29 @@ stem(orgIn)
 grid; grid minor
 xlabel('Column Number');ylabel('# of Likes');title('Original Input')
 %%%%%%%%%%%%%%%%% End Input Generation %%%%%%%%%%%%%%%%%%%%%%%
+% Heat map
+% # of adjuncent point taken into consideration for heat map
+newInd = 1:0.1:length; % new index used for interpolation
+heatmapVector = interp1(index, orgIn, newInd); % interpol genrate trangular shape
+% heatmapVector = movmean(heatmapVector, 8); % moving avg create spline style
+
+% Plot generated heat map curve
+ax(2) = subplot(212);
+hold on
+plot(newInd, heatmapVector, '-xm')
+title('Heat Map');xlabel('Column Position');ylabel('Heat');
+grid; grid minor
+
+linkaxes(ax, 'x') % link axis
+%%%%%%%%%%%%%%%% End Expected Output %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% length, index (for input data), orgIn
+%%%%%%%%%%%%%%%% Begin interpolation calculation %%%%%%%%%%%%%%
+pixInd = 0.1; % value obatined from how dense the pixals are, depending on screen size;
+hmInd = 1:pixInd:length; % generate heatmap index
+
+% create function hmHeight = mInterp(oriIndex, oriData, querryPt)
+outTest = mInterpl(index, orgIn, hmInd);
+plot(hmInd, outTest, '-ob')
+
+% Test if manually compute result match expected output
---
+++ matlab model/mInterpl.m
@@ -0,0 +1,21 @@
+function [qData] = mInterpl(orgId,orgData, qId)
+%mInterpl interpolation computation using semi-spline method
+%   [qData] = mInterpl(orgId,orgData, qId)
+    qData(1:length(qId)) = 0;
+    
+for i = 2:length(qId)-1
+    % get querry pt (single)
+    qPt = qId(i);
+    % LH and RH pts
+    lh_x = floor(qPt);  % x1
+    rh_x = ceil(qPt);   % x2
+    lh_y = orgData(orgId==lh_x);    % y1
+    rh_y = orgData(orgId==rh_x);    % y2
+    % Interpol to get data val at given pt
+    qDataPt = mSingleInterpl(lh_x, lh_y, rh_x, rh_y, qPt);
+    qData(i) = qDataPt;
+end
+
+
+end
+
